a313e0772c62feb3a5442ad47b2c4ff407b00084
Wout De Soete <wout.desoete@student.kuleuven.be>
2025-11-07 11:31:40 +0100

    start plab1

---
+++ startcodeplab1/datamgr.c
@@ -0,0 +1,15 @@
+// created by Wout De Soete
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+void datamgr_parse_sensor_files(FILE *fp_sensor_map, FILE *fp_sensor_data){
+
+
+}
